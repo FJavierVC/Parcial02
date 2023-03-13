ac24c4fc5fbb21298f9d38446074799766fc0202
Javier Valdez <Javier Valdez@LAPTOP-HGQSLD20>
2023-03-13 08:36:45 -0600

    Agregar archivos de proyecto.

---
+++ Practica02/Data/Automovil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica02.Data
+{
+    public class Automovil
+	{
+        
+		 #region:Propiedades\
+
+		public int id;
+		public string marca;
+		public string modelo;
+		public int km;
+        public string version;
+		public double precio;
+
+		#endregion
+
+		#region:Construuctor
+		public Automovil(int id, string marca, string modelo, int km, string version, double precio)
+        {
+            this.id = id;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.km = km;
+            this.version= version;
+            this.precio = precio;
+        }
+        #endregion
+	}
+}
+    
